fcd92d26256ea66f02a549121f49f6973829e1f3
Raul Sanchez <rawmind@gmail.com>
2016-02-02 22:16:00 +0100

    confd refactor to rancher-metadata v2

--- start.sh
+++ start.sh
@@ -30,10 +30,17 @@ function taillog {
     tail -F ${KAFKA_HOME}/logs/kafkaServer.out &
 }
 
+function rmconfd {
+    if [ -f /etc/monit/conf.d/confd.conf ]; then
+        rm /etc/monit/conf.d/confd.conf
+    fi
+}
+
 CONFD_BACKEND=${CONFD_BACKEND:-"rancher"}
-CONFD_PREFIX=${CONFD_PREFIX:-"/latest"}
+CONFD_PREFIX=${CONFD_PREFIX:-"/2015-12-19"}
 CONFD_INTERVAL=${CONFD_INTERVAL:-60}
 CONFD_PARAMS=${CONFD_PARAMS:-"-backend ${CONFD_BACKEND} -prefix ${CONFD_PREFIX}"}
+CONFD_ONETIME="/usr/bin/confd -onetime ${CONFD_PARAMS}"
 CONFD_SCRIPT=${CONFD_SCRIPT:-"/tmp/confd-start.sh"}
 KAFKA_HEAP_OPTS=${JVMFLAGS:-"-Xmx1G -Xms1G"}
 
@@ -44,20 +51,25 @@ taillog
 
 if [ "$CONFD_INTERVAL" -gt "0" ]; then
     CONFD_PARAMS="-interval ${CONFD_INTERVAL} ${CONFD_PARAMS}"
+
+    # Create confd start script
+    echo "#!/usr/bin/env sh" > ${CONFD_SCRIPT}
+    echo "/usr/bin/nohup /usr/bin/confd ${CONFD_PARAMS} > /opt/kafka/logs/confd.log 2>&1 &" >> ${CONFD_SCRIPT}
+    echo "rc=\$?" >> ${CONFD_SCRIPT}
+    echo "echo \$rc" >> ${CONFD_SCRIPT}
+    chmod 755 ${CONFD_SCRIPT}
+
+    log "[ Refreshing broker configuration every ${CONFD_INTERVAL} seconds... ]"
 else
-    CONFD_PARAMS="-onetime ${CONFD_PARAMS}"
+    #Execute confd in onepass. Removing confd monit script
+    log "[ Static broker configuration... ]"
+    rmconfd
 fi
 
-# Create confd start script
-echo "#!/usr/bin/env sh" > ${CONFD_SCRIPT}
-echo "/usr/bin/nohup /usr/bin/confd ${CONFD_PARAMS} > /opt/kafka/logs/confd.log 2>&1 &" >> ${CONFD_SCRIPT}
-echo "rc=\$?" >> ${CONFD_SCRIPT}
-echo "echo \$rc" >> ${CONFD_SCRIPT}
-chmod 755 ${CONFD_SCRIPT}
-
-# Run confd start script
-${CONFD_SCRIPT}
+# Run confd to get first appli configuration
+log "[ Getting broker configuration... ]"
+${CONFD_ONETIME}
 
 # Run monit
+log "[ Starting monit... ]"
 /usr/bin/monit -I
-
