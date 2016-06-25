d4c644de4f87a9081d630989945343b8aa6cc000
Raul Sanchez <rawmind@gmail.com>
2016-06-25 14:14:23 +0200

    update from base version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM rawmind/rancher-tools:0.3.4
+FROM rawmind/rancher-tools:0.3.4-2
 MAINTAINER Raul Sanchez <rawmind@gmail.com>
 
 #Set environment
--- root/opt/tools/confd/bin/service-conf.sh
+++ root/opt/tools/confd/bin/service-conf.sh
@@ -16,22 +16,21 @@ function checkNetwork {
     done
 
     log "[ Checking container connectivity... ]"
-    b="`ping -c 1 rancher-metadata. &> /dev/null; echo $?`"
+    b="`fping -c 1 rancher-metadata.rancher.internal &> /dev/null; echo $?`"
     while [ $b -eq 1 ]; 
     do
-        b="`ping -c 1 rancher-metadata. &> /dev/null; echo $?`"
+        b="`fping -c 1 rancher-metadata.rancher.internal &> /dev/null; echo $?`"
         sleep 1 
     done
 }
 
 function serviceTemplate {
     log "[ Checking ${CONF_NAME} template... ]"
-    if [ ! -f "/opt/tools/${CONF_NAME}/etc/templates/server.properties.tmpl" ]; then
-        sh ${CONF_HOME}/bin/server.properties.tmpl.sh
-    fi
+    sh ${CONF_HOME}/bin/server.properties.tmpl.sh
 }
 
 function serviceStart {
+    checkNetwork
     serviceTemplate
     log "[ Starting ${CONF_NAME}... ]"
     /usr/bin/nohup ${CONF_INTERVAL} > ${CONF_HOME}/log/confd.log 2>&1 &
@@ -46,6 +45,7 @@ function serviceRestart {
     log "[ Restarting ${CONF_NAME}... ]"
     serviceStop 
     serviceStart
+    /opt/monit/bin/monit reload
 }
 
 CONF_NAME=confd
